06066835dc85b0e831912a2367eb3a517a475fcc
ranjen <ranjen@myboost.com.my>
2019-07-29 17:33:40 +0800

    Functional Interface - Predicate Part 1 : Refactor code for Basic using test method

--- src/com/ranjen/functionalInterfaces/PredicateExample.java
+++ src/com/ranjen/functionalInterfaces/PredicateExample.java
@@ -4,19 +4,17 @@ import java.util.function.Predicate;
 
 public class PredicateExample {
 
+    //using Predicate to check if that number is even or not using test method
+
+    static Predicate<Integer> p = (i) ->{return (i%2==0);};
+
+    //You can simplify the above as below if it is single statement
+    static Predicate<Integer> p1 = (i) -> i%2==0;
+
     public static void main(String[] args) {
-
-        //using Predicate to check if that number is even or not using test method
-
-        Predicate<Integer> p = (i) ->{
-            return (i%2==0);
-        };
 
         System.out.println(p.test(4));
 
-        //You can simplify the above as below if it is single statement
-
-        Predicate<Integer> p1 = (i) -> i%2==0;
         System.out.println(p1.test(8));
 
     }
