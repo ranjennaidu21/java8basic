ad89e8878208b5fcc8fda9153efb15096c665c31
ranjen <ranjen@myboost.com.my>
2019-07-29 12:25:06 +0800

    Functional Interface - Consumer Part 2 : basic print all object

--- src/com/ranjen/functionalInterfaces/ConsumerExample.java
+++ src/com/ranjen/functionalInterfaces/ConsumerExample.java
@@ -1,5 +1,9 @@
 package com.ranjen.functionalInterfaces;
 
+import com.ranjen.data.Student;
+import com.ranjen.data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
@@ -8,6 +12,11 @@ public class ConsumerExample {
         Consumer<String> c = (s)->System.out.println(s.toUpperCase());
 
         c.accept("java8");
+
+        Consumer<Student> c2 = (student)->System.out.println(student);
+
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(c2); //it accepting Consumer Implementation, so we create one above
     }
 
 }
