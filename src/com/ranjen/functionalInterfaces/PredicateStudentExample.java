0001da31f0ce0285434627d9cc1807e95810fce0
ranjen <ranjen@myboost.com.my>
2019-07-29 18:36:30 +0800

    Functional Interface - Predicate Part 2 : Filter Student by grade and gpa using predicate

--- src/com/ranjen/functionalInterfaces/PredicateStudentExample.java
+++ src/com/ranjen/functionalInterfaces/PredicateStudentExample.java
@@ -8,17 +8,44 @@ import java.util.function.Predicate;
 
 public class PredicateStudentExample {
 
-    static Predicate<Student> p1 = (s)-> s.getGradeLevel()>=3.6;
+    static Predicate<Student> p1 = (s)-> s.getGradeLevel()>=3;
+    static Predicate<Student> p2 = (s)-> s.getGpa()>=3.9;
 
     public static void main(String[] args) {
         filterStudentByGrade();
+        filterStudentByGpa();
+        filterStudents();
     }
 
     public static void filterStudentByGrade(){
+        System.out.println("filterStudentByGrade()");
         List<Student> studentList = StudentDataBase.getAllStudents();
 
         studentList.forEach((student)->{
             if(p1.test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
+    public static void filterStudentByGpa(){
+        System.out.println("filterStudentByGpa()");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach((student)->{
+            if(p2.test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
+    //to filter both grade and gpa
+    public static void filterStudents(){
+        System.out.println("filterStudentsByGradeAndGpa()");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach((student)->{
+            if(p1.and(p2).test(student)){ //can use or , negate method as well
                 System.out.println(student);
             }
         });
