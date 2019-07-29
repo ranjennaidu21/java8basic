74efd1a19fe733822b818e3548b499aeac166ed6
ranjen <ranjen@myboost.com.my>
2019-07-29 12:16:06 +0800

    Functional Interface - Consumer Part 1

---
+++ src/com/ranjen/functionalInterfaces/ConsumerExample.java
@@ -0,0 +1,13 @@
+package com.ranjen.functionalInterfaces;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        Consumer<String> c = (s)->System.out.println(s.toUpperCase());
+
+        c.accept("java8");
+    }
+
+}
