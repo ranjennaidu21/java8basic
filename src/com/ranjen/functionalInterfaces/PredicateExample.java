4c9e00529049484ae5b933d2634bdbf68f18965c
ranjen <ranjen@myboost.com.my>
2019-07-29 17:45:27 +0800

    Functional Interface - Predicate Part 1 : Predicate basic using or,and and negate method

--- src/com/ranjen/functionalInterfaces/PredicateExample.java
+++ src/com/ranjen/functionalInterfaces/PredicateExample.java
@@ -11,12 +11,35 @@ public class PredicateExample {
     //You can simplify the above as below if it is single statement
     static Predicate<Integer> p1 = (i) -> i%2==0;
 
+    static Predicate<Integer> p2 = (i) -> i%5==0;
+
     public static void main(String[] args) {
 
         System.out.println(p.test(4));
 
         System.out.println(p1.test(8));
 
+        predicateAnd();
+        predicateOr();
+        predicateNegate();
+
+    }
+
+    //return true if both true
+    public static void predicateAnd(){
+        System.out.println("predicateAnd result is: "+ p1.and(p2).test(10));
+        System.out.println("predicateAnd Value is: "+ p1.and(p2).test(8));
+    }
+    //return true if either one is true
+    public static void predicateOr(){
+        System.out.println("predicateOr result is: "+ p1.or(p2).test(10));
+        System.out.println("predicateOr Value is: "+ p1.or(p2).test(8));
+    }
+
+    //give the opposite result , if true return false and vice versa
+    public static void predicateNegate(){
+        System.out.println("predicateNegate result is: "+ p1.or(p2).negate().test(10));
+        System.out.println("predicateNegate Value is: "+ p1.or(p2).negate().test(8));
     }
 
 }
