b715a56892fa8708fb4279a86c667d7af4c73027
ranjen <ranjen@myboost.com.my>
2019-07-30 22:36:21 +0800

    Stream Operation - distinct,count,sorted functions

--- src/com/ranjen/streams/StreamFlatMapExample.java
+++ src/com/ranjen/streams/StreamFlatMapExample.java
@@ -15,13 +15,47 @@ public class StreamFlatMapExample {
     public static List<String> getStudentActivites(){
         List<String> studentActivitiesList = StudentDataBase.getAllStudents().stream() //Stream<Student>
                 .map(Student::getActivities) //Stream<List<String>>
-                .flatMap(List::stream) //need to flat the map as above it is still Stream<List<String>>, need to be Stream<String>
+                .flatMap(List::stream) //need to flat the stream as above it is still Stream<List<String>>, need to be Stream<String>
+                .collect(Collectors.toList()); //once it is one element Stream<String> the only able to pass to collect
+        return studentActivitiesList;
+    }
+
+    //with distinct function performed
+    public static List<String> getDiffStudentActivites(){
+        List<String> studentActivitiesList = StudentDataBase.getAllStudents().stream() //Stream<Student>
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) //need to flat the stream as above it is still Stream<List<String>>, need to be Stream<String>
+                .distinct() // now it will ignore all the repetitive elements
+                .collect(Collectors.toList()); //once it is one element Stream<String> the only able to pass to collect
+        return studentActivitiesList;
+    }
+
+    //with count function performed
+    public static long getStudentActivitesCount(){
+        long studentActivitiesCount = StudentDataBase.getAllStudents().stream() //Stream<Student>
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) //need to flat the stream as above it is still Stream<List<String>>, need to be Stream<String>
+                .distinct() // now it will ignore all the repetitive elements
+                .count();
+        return studentActivitiesCount;
+    }
+
+    //with sorted function performed - sorted in alphabetical order
+    public static List<String> getDiffStudentActivitesSorted(){
+        List<String> studentActivitiesList = StudentDataBase.getAllStudents().stream() //Stream<Student>
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) //need to flat the stream as above it is still Stream<List<String>>, need to be Stream<String>
+                .distinct() // now it will ignore all the repetitive elements
+                .sorted()
                 .collect(Collectors.toList()); //once it is one element Stream<String> the only able to pass to collect
         return studentActivitiesList;
     }
 
     public static void main(String[] args) {
         System.out.println(getStudentActivites());
+        System.out.println(getDiffStudentActivites());
+        System.out.println(getStudentActivitesCount());
+        System.out.println(getDiffStudentActivitesSorted());
     }
 
 }
